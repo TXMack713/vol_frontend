99135a3dd25e772726dce844345aed0938937d73
Anthony Mack <49073356+TXMack713@users.noreply.github.com>
2020-03-31 20:42:50 -0500

    commented out the /build folder in the .gitignore file

---
+++ build/precache-manifest.b18531922d8eaff793dca74d1f8d23d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e737875ef28bfda9e99560a6f177c777",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a7c18fe172b5f1bf189",
+    "url": "/static/js/2.3361fc83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3361fc83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d1d4da8e713244e0f21",
+    "url": "/static/js/main.2285410c.chunk.js"
+  },
+  {
+    "revision": "36a8dfbca075216e19d6",
+    "url": "/static/js/runtime-main.3f9aea6f.js"
+  }
+]);
